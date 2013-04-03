c13deb3f3a7b9e96a329d64b9c882fdb8c4eed1c
Steven Sloan <stevenosloan@gmail.com>
2013-04-03 00:40:15 -0400

    Deployment::Branches is created with the current branch as public_dir

--- lib/statistrano/deployment/branches.rb
+++ lib/statistrano/deployment/branches.rb
@@ -2,6 +2,22 @@ module Statistrano
   module Deployment
 
     class Branches < Base
+
+      class Config < Base::Config
+        attr_accessor :public_dir
+
+        def initialize
+          yield(self) if block_given?
+        end
+      end
+
+      def initialize name
+        super name
+        @config = Config.new do |c|
+          c.public_dir = Git.current_branch.to_slug
+        end
+      end
+
     end
 
   end
