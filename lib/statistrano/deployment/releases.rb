0cfc9242e9f8fa1927a881af46caf42defe2e850
Steven Sloan <stevenosloan@gmail.com>
2013-04-05 11:21:15 -0400

    add release management to Deployment::Releases

--- lib/statistrano/deployment/releases.rb
+++ lib/statistrano/deployment/releases.rb
@@ -22,11 +22,46 @@ module Statistrano
         end
       end
 
+      def deploy
+        super
+        prune_releases
+      end
+
+      # Rollback to the previous release
+      # @return [Void]
+      def rollback_release
+        releases = get_releases
+
+        if releases.length > 1
+          current_release = releases[0]
+          past_release = releases[1]
+
+          symlink_release( past_release )
+          remove_release( current_release )
+
+        else
+          LOG.error "Whoa there, there's only one release -- you definetly shouldn't remove it"
+        end
+      end
+
+      # Remove old releases
+      # @return [Void]
+      def prune_releases
+        releases = get_releases
+        if releases && releases.length > @config.release_count
+          releases[@config.release_count..-1].each do |release|
+            remove_release(release)
+          end
+        else
+          LOG.msg "No releases to prune", nil
+        end
+      end
+
       # Return array of releases on the remote
       # @return [Array]
-      def list_releases
+      def get_releases
         releases = []
-        run_ssh_command("ls -m #{release_path}") do |ch, stream, data|
+        run_ssh_command("ls -m #{release_dir_path}") do |ch, stream, data|
           releases = data.strip.split
         end
         return releases
@@ -34,9 +69,57 @@ module Statistrano
 
       private
 
+        # send code to remote server
+        # @return [Void]
+        def create_release
+          current_release = release_name
+
+          setup_release_dir_path( release_path(current_release) )
+          rsync_to_remote( release_path(current_release) )
+          symlink_release( current_release )
+
+          # TODO: add_manifest
+
+          LOG.msg "Created release at #{public_dir}"
+        end
+
+        # remove a release
+        # @param name [String]
+        # @return [Void]
+        def remove_release name
+          LOG.msg "Removing release '#{release}'"
+          run_ssh_command "rm -rf #{release_dir_path}/#{name}"
+        end
+
+        # Symlink a release to the public path
+        # @param name [String]
+        # @return [Void]
+        def symlink_release name
+          "ln -nfs #{release_path(name)} #{public_path}"
+        end
+
+        # Return a release name based on current time
+        # @return [String]
+        def release_name
+          Time.now.strftime("%Y%m%d%H%M%S")
+        end
+
+        # Full public_path
+        # @return [String]
+        def public_path
+          File.join( @config.remote_dir, @config.public_dir )
+        end
+
+        # Full path to a release
+        # @param name [String] name of the release
+        # @return [String] full path to the release
+        def release_path name
+          File.join( release_dir_path, name )
+        end
+
         # Full path to release directory
         # @return [String]
-        def release_path
+        def release_dir_path
           File.join( @config.remote_dir, @config.release_dir )
         end
 
