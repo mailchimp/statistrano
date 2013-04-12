d1e15ca8e9b6e05eabefec97cd1c8f4fe8d5051b
Steven Sloan <stevenosloan@gmail.com>
2013-04-12 14:08:25 -0400

    start on pruning for branches

--- lib/statistrano/deployment/branches.rb
+++ lib/statistrano/deployment/branches.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 module Statistrano
   module Deployment
 
@@ -14,7 +14,8 @@ module Statistrano
 
         def tasks
           super.merge({
-            :list => :list_releases
+            :list => :list_releases,
+            :prune => :prune_releases
           })
         end
       end
@@ -37,17 +38,35 @@ module Statistrano
           LOG.msg "#{r.name} created at #{Time.at(r.time).strftime('%a %b %d, %Y at %l:%M %P')}"
         end
       end
+
+      def prune_releases
+        releases = get_releases
+
+        get_actual_releases.each do |r|
+          remove_release(r) unless get_releases.include? r
+        end
+      end
+
       private
 
         # send code to remote server
         # @return [Void]
         def create_release
-          setup_release_path(release_path)
-          rsync_to_remote(release_path)
+          setup_release_path(current_release_path)
+          rsync_to_remote(current_release_path)
 
           @manifest.add_release( Manifest::Release.new( @config.public_dir ) )
 
           LOG.msg "Created release at #{@config.public_dir}"
+        end
+
+        # remove a release
+        # @param name [String]
+        # @return [Void]
+        def remove_release name
+          LOG.msg "Removing release '#{name}'"
+          @ssh.run_command "rm -rf #{release_path(name)}"
+          @manifest.remove_release(name)
         end
 
         # return array of releases from the manifest
@@ -56,11 +75,28 @@ module Statistrano
           @manifest.list
         end
 
+        # return array of releases on the remote
+        # @return [Array]
+        def get_actual_releases
+          releases = []
+          @ssh.run_command("ls -mp #{@config.remote_dir}") do |ch, stream, data|
+            releases = data.strip.split(',')
+          end
+          releases.keep_if { |r| /\/$/.match(r) }
+          releases.map { |r| r.strip.gsub(/(\/$)/, '') }.keep_if { |r| r != "index" }
+        end
+
         # path to the current release
         # this is based on the git branch
         # @return [String]
-        def release_path
+        def current_release_path
           File.join( @config.remote_dir, @config.public_dir )
+        end
+
+        # path to a specific release
+        # @return [String]
+        def release_path name
+          File.join( @config.remote_dir, name )
         end
 
     end
