e03ed7ef54ce2a9e50d393c874d823d0746f1851
Steven Sloan <stevenlosloan@gmail.com>
2013-06-03 00:23:01 -0400

    add a shell module

---
+++ lib/statistrano/shell.rb
@@ -0,0 +1,27 @@
+require 'open3'
+
+module Statistrano
+  module Shell
+
+    class << self
+
+      def run command, &block
+        stdout, stderr, status = Open3.capture3(command)
+        STDERR.puts(stderr)
+
+        if status.success?
+          yield stdout if block_given?
+          true
+        else
+          STDERR.puts "Problem running #{command}"
+          false
+        end
+      rescue StandardError => e
+        STDERR.puts "Problem running '#{command}'"
+        STDERR.puts "Error: #{e}"
+      end
+
+    end
+
+  end
+end
