77cd3faa7cda16cc3048f6e84b487c4db100c6dc
Steven Sloan <stevenosloan@gmail.com>
2013-06-21 01:19:37 -0400

    stupidly naive integration test for base deployment

---
+++ spec/integration_tests/base_integration_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'fileutils'
+
+describe "deployment with Base" do
+
+  def pick_fixture name
+    root = Dir.pwd
+    Dir.chdir( File.join( root, "fixture", name ) )
+  end
+
+  after(:each) do
+    # cleanup the deployment
+    FileUtils.rm_rf File.join( Dir.getwd, "deployment" )
+  end
+
+  it "deploys the contents of source to the 'deploy' folder" do
+    pick_fixture "base_site"
+    Statistrano::Shell.run "rake local:deploy"
+    result_a, source = Statistrano::Shell.run("ls source")
+    result_b, deployment = Statistrano::Shell.run("ls deployment")
+    result_a.should be_true
+    result_b.should be_true
+    deployment.should == source
+  end
+
+end
