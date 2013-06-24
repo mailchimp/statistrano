8aa09d7e24b652545bb6ad8a1a25071e26c468f5
Steven Sloan <stevenosloan@gmail.com>
2013-06-24 00:49:53 -0400

    refactor tests a tad for releases, add test for symlinking

--- spec/integration_tests/releases_spec.rb
+++ spec/integration_tests/releases_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 include ::Rake::DSL
 namespace :remote do
   task :copy do
+    puts "build task running"
     `cp -r source/ build/ 2> /dev/null`
   end
 end
@@ -13,7 +14,11 @@ $stdout = output
 
 describe "Releases deployment integration test" do
 
-  before(:each) do
+  def tracer
+    puts "============================"
+  end
+
+  before(:all) do
     pick_fixture "releases_site"
     define_deployment "local", :releases do |c|
       c.build_task = 'remote:copy'
@@ -21,6 +26,14 @@ describe "Releases deployment integration test" do
       c.local_dir = 'build'
       c.remote_dir = File.join( Dir.pwd, 'deployment' )
     end
+  end
+
+  before(:each) do
+    pick_fixture "releases_site"
+    time = Time.at(1372020000)
+    Timecop.freeze(time)
+    Rake::Task["local:deploy"].reenable
+    Rake::Task["remote:copy"].reenable
   end
 
   let :deployment do
@@ -33,32 +46,45 @@ describe "Releases deployment integration test" do
   end
 
   after(:each) do
+    Timecop.return
     cleanup_fixture
   end
 
   describe ":deploy" do
     it "generates releases with the correct timestamp" do
-      time = Time.at(1372020000)
-      Timecop.freeze(time)
       Rake::Task["local:deploy"].invoke
 
-      time = Time.at(1372030000)
-      Timecop.travel(time)
       Rake::Task["local:deploy"].reenable
       Rake::Task["remote:copy"].reenable
+      Timecop.travel(10000)
       Rake::Task["local:deploy"].invoke
 
       Dir[ "deployment/releases/**" ].map { |d| d.gsub("deployment/releases/", '' ) }.should == ["1372020000","1372030000"]
     end
+
+    it "symlinks the pub_dir to the most recent release" do
+      Rake::Task["local:deploy"].invoke
+
+      status, stdout = Statistrano::Shell.run("ls -l deployment")
+      stdout.should =~ /current -> #{Dir.pwd.gsub("/", "\/")}\/deployment\/releases\/1372020000/
+    end
   end
 
   describe ":list" do
+    it "lists the previous releases" do
+    end
   end
 
   describe ":prune" do
+    it "doesn't remove releases under the max count" do
+    end
+    it "removes old releases outside the max count" do
+    end
   end
 
   describe ":rollback" do
+    it "symlinks the previous release and removes the most current" do
+    end
   end
 
 end
