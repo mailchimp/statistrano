2b709e3a8380d2ad2c8164ce0e64d2497c6c9fc7
Steven Sloan <stevenosloan@gmail.com>
2013-11-05 12:52:00 -0500

    move Manifest::Release and Manifest::RemoteStore to sep files

--- lib/statistrano/deployment/manifest.rb
+++ lib/statistrano/deployment/manifest.rb
@@ -1,3 +1,6 @@
+require_relative 'manifest/release'
+require_relative 'manifest/remote_store'
+
 module Statistrano
   module Deployment
 
@@ -9,9 +12,9 @@ module Statistrano
 
       def initialize config
         @config = config
-        @ssh = config.ssh_session
+        @ssh    = config.ssh_session
         @remote_store = RemoteStore.new( @config )
-        @releases = @remote_store.fetch
+        @releases     = @remote_store.fetch
       end
 
       def releases
@@ -56,121 +59,12 @@ module Statistrano
         @remote_store.update_content releases_as_json
       end
 
-      #
-      # Manages the state of a single release for the manifest
-      #
-      class Release
-
-        attr_reader :name, :config, :options
-
-        # init a release
-        # @param name [String] name of the release
-        # @param config [Obj] the config object
-        # @param options [Hash] :time, :commit, & :link || :repo_url
-        def initialize name, config, options={}
-          @name    = name
-          @config  = config
-          @options = convert_string_keys_to_symbols(options)
-        end
-
-        def time
-          @_time ||= options.fetch(:time) { Time.now.to_i }
-        end
-
-        def commit
-          @_commit ||= options.fetch(:commit) { Git.current_commit }
-        end
-
-        def link
-          @_link ||= options.fetch(:link) { (options[:repo_url]) ? "#{options[:repo_url]}/tree/#{commit}" : nil }
-        end
-
-        def log_info
-          LOG.msg "#{name} created at #{Time.at(time).strftime('%a %b %d, %Y at %l:%M %P')}"
-        end
-
-        # convert the release to an li element
-        # @return [String]
-        def as_li
-          "<li>" +
-          "<a href=\"http://#{name}.#{config.base_domain}\">#{name}</a>" +
-          "<small>updated: #{Time.at(time).strftime('%A %b %d, %Y at %l:%M %P')}</small>" +
-          "</li>"
-        end
-
-        # convert the release to a json object
-        # @return [String]
-        def to_json
-          hash = {
-            name: name,
-            time: time,
-            commit: commit
-          }
-          hash.merge({ link: link }) if link
-
-          return hash.to_json
-        end
-
-        private
-
-          def convert_string_keys_to_symbols hash
-            hash.inject({}) do |opts,(k,v)|
-              opts[k.to_sym] = v
-              opts
-            end
-          end
-      end
-
       private
 
         # json array of the releases
         # @return [String]
         def releases_as_json
           "[" << @releases.map { |release| release.to_json }.join(",") << "]"
-        end
-
-        # manages the manifest file on the remote
-        #
-        class RemoteStore
-
-          attr_reader :config, :path
-
-          def initialize config
-            @config = config
-            @path   = config.remote_dir
-          end
-
-          def fetch
-            fetch_remote_manifest.map { |release| new_release_instance( release ) }
-          end
-
-          def update_content string
-            config.ssh_session
-              .run "touch #{manifest_path} && echo '#{string}' > #{manifest_path}"
-          end
-
-          private
-
-            # path to the manifest
-            # @return [String]
-            def manifest_path
-              File.join( path, 'manifest.json' )
-            end
-
-            def fetch_remote_manifest
-              raw_manifest = config.ssh_session.run "touch #{manifest_path} && cat #{manifest_path}"
-              if raw_manifest.success? && !raw_manifest.stdout.empty?
-                return JSON.parse( raw_manifest.stdout )
-              else
-                return []
-              end
-            end
-
-            def new_release_instance release
-              name = release.delete("name")
-              release.merge({ repo_url: config.repo_url }) if config.repo_url
-              return Release.new( name, config, release )
-            end
         end
     end
 
---
+++ lib/statistrano/deployment/manifest/release.rb
@@ -0,0 +1,72 @@
+module Statistrano
+  module Deployment
+    class Manifest
+
+      #
+      # Manages the state of a single release for the manifest
+      #
+      class Release
+
+        attr_reader :name, :config, :options
+
+        # init a release
+        # @param name [String] name of the release
+        # @param config [Obj] the config object
+        # @param options [Hash] :time, :commit, & :link || :repo_url
+        def initialize name, config, options={}
+          @name    = name
+          @config  = config
+          @options = convert_string_keys_to_symbols(options)
+        end
+
+        def time
+          @_time ||= options.fetch(:time) { Time.now.to_i }
+        end
+
+        def commit
+          @_commit ||= options.fetch(:commit) { Git.current_commit }
+        end
+
+        def link
+          @_link ||= options.fetch(:link) { (options[:repo_url]) ? "#{options[:repo_url]}/tree/#{commit}" : nil }
+        end
+
+        def log_info
+          LOG.msg "#{name} created at #{Time.at(time).strftime('%a %b %d, %Y at %l:%M %P')}"
+        end
+
+        # convert the release to an li element
+        # @return [String]
+        def as_li
+          "<li>" +
+          "<a href=\"http://#{name}.#{config.base_domain}\">#{name}</a>" +
+          "<small>updated: #{Time.at(time).strftime('%A %b %d, %Y at %l:%M %P')}</small>" +
+          "</li>"
+        end
+
+        # convert the release to a json object
+        # @return [String]
+        def to_json
+          hash = {
+            name: name,
+            time: time,
+            commit: commit
+          }
+          hash.merge({ link: link }) if link
+
+          return hash.to_json
+        end
+
+        private
+
+          def convert_string_keys_to_symbols hash
+            hash.inject({}) do |opts,(k,v)|
+              opts[k.to_sym] = v
+              opts
+            end
+          end
+      end
+
+    end
+  end
+end
