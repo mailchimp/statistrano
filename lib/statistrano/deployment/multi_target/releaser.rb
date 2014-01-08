99f1e82963177478447dfa0e620a40345a4a0286
Steven Sloan <stevenosloan@gmail.com>
2014-01-08 15:27:02 -0500

    require a remote_dir to be set for MultiTarget::Releaser

--- lib/statistrano/deployment/multi_target/releaser.rb
+++ lib/statistrano/deployment/multi_target/releaser.rb
@@ -5,6 +5,7 @@ module Statistrano
       class Releaser
         extend ::Statistrano::Config::Configurable
 
+        option :remote_dir
         option :release_count, 5
         option :release_dir, "releases"
         option :public_dir,  "current"
@@ -16,6 +17,7 @@ module Statistrano
             config.send opt, options.fetch(opt,val)
           end
 
+          raise ArgumentError, "a remote_dir is required" unless config.remote_dir
           @release_name = Time.now.to_i.to_s
         end
 
--- spec/lib/statistrano/deployment/multi_target/releaser_spec.rb
+++ spec/lib/statistrano/deployment/multi_target/releaser_spec.rb
@@ -4,19 +4,25 @@ describe Statistrano::Deployment::MultiTarget::Releaser do
 
   describe "#initialize" do
     it "assigns options hash to configuration" do
-      subject = described_class.new release_count: 10
+      subject = described_class.new remote_dir: '/var/www/proj', release_count: 10
       expect( subject.config.release_count ).to eq 10
     end
 
     it "uses config.options defaults if option not given" do
-      subject = described_class.new release_count: 10
+      subject = described_class.new remote_dir: '/var/www/proj', release_count: 10
       expect( subject.config.release_dir ).to eq "releases"
+    end
+
+    it "requires a remote_dir to be set" do
+      expect{
+        described_class.new release_count: 10
+      }.to raise_error ArgumentError, "a remote_dir is required"
     end
 
     it "generates release_name from current time" do
       time = Time.now
       allow( Time ).to receive(:now).and_return(time)
-      subject = described_class.new
+      subject = described_class.new remote_dir: '/var/www/proj'
 
       allow( Time ).to receive(:now).and_return(time + 1)
       expect( Time.now ).not_to eq time # ensure that the time + 1 works
