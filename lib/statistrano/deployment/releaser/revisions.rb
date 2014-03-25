aa4494929f701f8055f437772a2e36706571b20e
Steven Sloan <stevenosloan@gmail.com>
2014-03-25 10:43:23 -0400

    sub "target" => "remote" in /revisions.rb

--- lib/statistrano/deployment/releaser/revisions.rb
+++ lib/statistrano/deployment/releaser/revisions.rb
@@ -22,43 +22,43 @@ module Statistrano
           @release_name = Time.now.to_i.to_s
         end
 
-        def create_release target, build_data={}
-          setup_release_path      target
-          rsync_to_remote         target
-          symlink_release         target
-          add_release_to_manifest target, build_data
-          prune_releases          target
+        def create_release remote, build_data={}
+          setup_release_path      remote
+          rsync_to_remote         remote
+          symlink_release         remote
+          add_release_to_manifest remote, build_data
+          prune_releases          remote
         end
 
-        def setup_release_path target
-          current_release = current_release target
-          target.create_remote_dir releases_path(target)
+        def setup_release_path remote
+          current_release = current_release remote
+          remote.create_remote_dir releases_path(remote)
 
           if current_release.empty?
-            target.create_remote_dir release_path(target)
+            remote.create_remote_dir release_path(remote)
           else
-            target.run "cp -a #{release_path(target, current_release)} #{release_path(target)}"
+            remote.run "cp -a #{release_path(remote, current_release)} #{release_path(remote)}"
           end
         end
 
-        def rsync_to_remote target
-          resp = target.rsync_to_remote local_path(target), release_path(target)
+        def rsync_to_remote remote
+          resp = remote.rsync_to_remote local_path(remote), release_path(remote)
           unless resp.success?
             abort()
           end
         end
 
-        def symlink_release target, release=nil
-          target.run "ln -nfs #{release_path(target, release)} #{public_path(target)}"
+        def symlink_release remote, release=nil
+          remote.run "ln -nfs #{release_path(remote, release)} #{public_path(remote)}"
         end
 
-        def prune_releases target
-          remove_untracked_releases target
-          remove_releases_beyond_release_count target
+        def prune_releases remote
+          remove_untracked_releases remote
+          remove_releases_beyond_release_count remote
         end
 
-        def list_releases target
-          manifest = new_manifest target
+        def list_releases remote
+          manifest = new_manifest remote
           manifest.data.keep_if do |rel|
             rel.has_key?(:release)
           end.sort_by do |rel|
@@ -66,72 +66,72 @@ module Statistrano
           end.reverse
         end
 
-        def rollback_release target
-          manifest = new_manifest target
-          releases = tracked_releases target, manifest
+        def rollback_release remote
+          manifest = new_manifest remote
+          releases = tracked_releases remote, manifest
 
           unless releases.length > 1
             return Log.error "There is only one release, best not to remove it"
           end
 
-          symlink_release target, releases[1]
-          remove_release releases[0], target, manifest
+          symlink_release remote, releases[1]
+          remove_release releases[0], remote, manifest
           manifest.save!
         end
 
-        def add_release_to_manifest target, build_data={}
-          manifest = new_manifest target
+        def add_release_to_manifest remote, build_data={}
+          manifest = new_manifest remote
           manifest.push build_data.merge(release: release_name)
           manifest.save!
         end
 
         private
 
-          def new_manifest target
-            Strategy::Releases::Manifest.new target_overridable_config(:remote_dir, target), target
+          def new_manifest remote
+            Strategy::Releases::Manifest.new remote_overridable_config(:remote_dir, remote), remote
           end
 
-          def target_overridable_config option, target
-            (target && target.config.public_send(option)) || config.public_send(option)
+          def remote_overridable_config option, remote
+            (remote && remote.config.public_send(option)) || config.public_send(option)
           end
 
-          def remove_releases_beyond_release_count target
-            manifest = new_manifest target
-            beyond   = tracked_releases(target, manifest)[target_overridable_config(:release_count, target)..-1]
+          def remove_releases_beyond_release_count remote
+            manifest = new_manifest remote
+            beyond   = tracked_releases(remote, manifest)[remote_overridable_config(:release_count, remote)..-1]
             Array(beyond).each do |beyond|
-              remove_release beyond, target, manifest
+              remove_release beyond, remote, manifest
             end
             manifest.save!
           end
 
-          def remove_release release_name, target, manifest
-            if release_name == current_release(target)
+          def remove_release release_name, remote, manifest
+            if release_name == current_release(remote)
               Log.warn "did not remove release '#{release_name}' because it is current"
               return
             end
 
             manifest.remove_if { |r| r[:release] == release_name }
-            target.run("rm -rf #{File.join(releases_path(target), release_name)}")
+            remote.run("rm -rf #{File.join(releases_path(remote), release_name)}")
           end
 
-          def remove_untracked_releases target
-            (remote_releases(target) - tracked_releases(target)).each do |untracked|
-              target.run("rm -rf #{File.join(releases_path(target), untracked)}")
+          def remove_untracked_releases remote
+            (remote_releases(remote) - tracked_releases(remote)).each do |untracked|
+              remote.run("rm -rf #{File.join(releases_path(remote), untracked)}")
             end
           end
 
-          def current_release target
-            resp = target.run("readlink #{public_path(target)}")
-            resp.stdout.sub( /#{releases_path(target)}\/?/, '' ).strip
+          def current_release remote
+            resp = remote.run("readlink #{public_path(remote)}")
+            resp.stdout.sub( /#{releases_path(remote)}\/?/, '' ).strip
           end
 
-          def remote_releases target
-            target.run("ls -m #{releases_path(target)}").stdout
+          def remote_releases remote
+            remote.run("ls -m #{releases_path(remote)}").stdout
                   .split(',').map(&:strip)
           end
 
-          def tracked_releases target, manifest=nil
-            manifest ||= new_manifest target
+          def tracked_releases remote, manifest=nil
+            manifest ||= new_manifest remote
             manifest.data.map do |data|
               data.fetch(:release, nil)
             end.compact.sort.reverse
@@ -143,21 +143,21 @@ module Statistrano
             end
           end
 
-          def local_path target=nil
-            File.join( Dir.pwd, target_overridable_config(:local_dir, target) )
+          def local_path remote=nil
+            File.join( Dir.pwd, remote_overridable_config(:local_dir, remote) )
           end
 
-          def releases_path target=nil
-            File.join( target_overridable_config(:remote_dir, target), target_overridable_config(:release_dir, target) )
+          def releases_path remote=nil
+            File.join( remote_overridable_config(:remote_dir, remote), remote_overridable_config(:release_dir, remote) )
           end
 
-          def release_path target=nil, release=nil
+          def release_path remote=nil, release=nil
             release ||= release_name
-            File.join( releases_path(target), release )
+            File.join( releases_path(remote), release )
           end
 
-          def public_path target=nil
-            File.join( target_overridable_config(:remote_dir, target), target_overridable_config(:public_dir, target) )
+          def public_path remote=nil
+            File.join( remote_overridable_config(:remote_dir, remote), remote_overridable_config(:public_dir, remote) )
           end
 
       end
