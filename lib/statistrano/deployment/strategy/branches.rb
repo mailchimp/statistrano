bd6d6006fdb635556d10556539c86f4b6fc9cf04
Steven Sloan <stevenosloan@gmail.com>
2014-12-05 17:38:45 -0500

    get Branches supporting multiple remotes

--- lib/statistrano/deployment/strategy/branches.rb
+++ lib/statistrano/deployment/strategy/branches.rb
@@ -24,8 +24,12 @@ module Statistrano
         # output a list of the releases in manifest
         # @return [Void]
         def list_releases
-          sorted_release_data.each do |release|
-            Log.info "#{release[:name]} created at #{Time.at(release[:time]).strftime('%a %b %d, %Y at %l:%M %P')}"
+          remotes.each do |remote|
+            releases_data = sorted_release_data(remote)
+            releases_data.map! do |release|
+              "#{release[:name]} created at #{Time.at(release[:time]).strftime('%a %b %d, %Y at %l:%M %P')}"
+            end
+            Log.info remote.config.hostname.to_sym, *releases_data
           end
         end
 
@@ -33,10 +37,15 @@ module Statistrano
         # get user input for removal of other releases
         # @return [Void]
         def prune_releases
-          prune_untracked_releases
+          candidate_releases = []
+          remotes.each do |remote|
+            prune_untracked_releases(remote)
+            candidate_releases.push *get_releases(remote)
+            candidate_releases.uniq!
+          end
 
-          if get_releases && get_releases.length > 0
-            pick_and_remove_release
+          if candidate_releases.length > 0
+            pick_and_remove_release candidate_releases
           else
             Log.warn "no releases to prune"
           end
@@ -45,71 +54,80 @@ module Statistrano
         # generate an index file for releases in the manifest
         # @return [Void]
         def generate_index
-          index_dir  = File.join( config.remote_dir, "index" )
-          index_path = File.join( index_dir, "index.html" )
-          remote.create_remote_dir index_dir
-          remote.run "touch #{index_path} && echo '#{release_list_html}' > #{index_path}"
+          remotes.each do |remote|
+            index_dir  = File.join( remote_overridable_config(:remote_dir, remote), "index" )
+            index_path = File.join( index_dir, "index.html" )
+            remote.create_remote_dir index_dir
+            remote.run "touch #{index_path} && echo '#{release_list_html(remote)}' > #{index_path}"
+          end
         end
 
         # push the current release into the manifest
         # @return [Void]
         def push_current_release_to_manifest
-          manifest.put Release.new( config.public_dir, config ).to_hash, :name
-          manifest.save!
+          remotes.each do |remote|
+            mnfst = manifest(remote)
+            mnfst.put Release.new( config.public_dir, config ).to_hash, :name
+            mnfst.save!
+          end
         end
 
         private
 
-          def remote
-            remotes.first
-          end
+          def manifest remote
+            @_manifests ||= {}
 
-          def manifest
-            @_manifest ||= Deployment::Manifest.new remote_overridable_config(:remote_dir, remote), remote
+            @_manifests.fetch( remote ) do
+              Deployment::Manifest.new remote_overridable_config(:remote_dir, remote), remote
+            end
           end
 
           def remote_overridable_config option, remote
             (remote && remote.config.public_send(option)) || config.public_send(option)
           end
 
-          def pick_and_remove_release
-            picked_release = pick_release_to_remove
+          def pick_and_remove_release candidate_releases
+            picked_release = pick_release_to_remove candidate_releases
             if picked_release
-              remove_release(picked_release)
+              remotes.each do |remote|
+                if get_releases(remote).include? picked_release
+                  remove_release(remote, picked_release)
+                end
+              end
               generate_index
             else
               Log.warn "sorry, that isn't one of the releases"
             end
           end
 
-          def pick_release_to_remove
-            list_releases_with_index
+          def pick_release_to_remove candidate_releases
+            list_releases_with_index candidate_releases
 
             picked_release = Shell.get_input("select a release to remove: ").gsub(/[^0-9]/, '')
 
-            if !picked_release.empty? && picked_release.to_i < get_releases.length
-              return get_releases[picked_release.to_i]
+            if !picked_release.empty? && picked_release.to_i < candidate_releases.length
+              return candidate_releases[picked_release.to_i]
             else
               return false
             end
           end
 
-          def list_releases_with_index
-            get_releases.each_with_index do |release,idx|
+          def list_releases_with_index releases
+            releases.each_with_index do |release,idx|
               Log.info :"[#{idx}]", "#{release}"
             end
           end
 
           # removes releases that are on the remote but not in the manifest
           # @return [Void]
-          def prune_untracked_releases
-            get_actual_releases.each do |release|
-              remove_release(release) unless get_releases.include? release
+          def prune_untracked_releases remote
+            get_actual_releases(remote).each do |release|
+              remove_release(remote, release) unless get_releases(remote).include? release
             end
           end
 
-          def release_list_html
-            releases = sorted_release_data.map do |r|
+          def release_list_html remote
+            releases = sorted_release_data(remote).map do |r|
               name = r.fetch(:name)
               r.merge({ repo_url: config.repo_url }) if config.repo_url
               Release.new( name, config, r )
@@ -118,8 +136,8 @@ module Statistrano
             Index.new( releases ).to_html
           end
 
-          def sorted_release_data
-            manifest.data.sort_by do |r|
+          def sorted_release_data remote
+            manifest(remote).data.sort_by do |r|
               r[:time]
             end.reverse
           end
@@ -127,29 +145,29 @@ module Statistrano
           # remove a release
           # @param name [String]
           # @return [Void]
-          def remove_release name
-            Log.info "Removing release '#{name}'"
-            remote.run "rm -rf #{release_path(name)}"
-            manifest.remove_if do |r|
+          def remove_release remote, name
+            Log.info remote.config.hostname.to_sym, "Removing release '#{name}'"
+            remote.run "rm -rf #{release_path(name, remote)}"
+            manifest(remote).remove_if do |r|
               r[:name] == name
             end
-            manifest.save!
+            manifest(remote).save!
           end
 
           # return array of releases from the manifest
           # @return [Array]
-          def get_releases
-           sorted_release_data.map { |r| r[:name] }
+          def get_releases remote
+           sorted_release_data(remote).map { |r| r[:name] }
           end
 
           # return array of releases on the remote
           # @return [Array]
-          def get_actual_releases
-            releases = []
-            resp = remote.run("ls -mp #{config.remote_dir}")
-            releases = resp.stdout.strip.split(',')
-            releases.keep_if { |release| /\/$/.match(release) }
-            releases.map { |release| release.strip.gsub(/(\/$)/, '') }.keep_if { |release| release != "index" }
+          def get_actual_releases remote
+            remote.run("ls -mp #{remote_overridable_config(:remote_dir, remote)}")
+                  .stdout.strip.split(',')
+                  .keep_if { |release| /\/$/.match(release) }
+                  .map     { |release| release.strip.sub(/(\/$)/, '') }
+                  .keep_if { |release| release != "index" }
           end
 
           # path to the current release
@@ -161,8 +179,8 @@ module Statistrano
 
           # path to a specific release
           # @return [String]
-          def release_path name
-            File.join( config.remote_dir, name )
+          def release_path name, remote
+            File.join( remote_overridable_config(:remote_dir, remote), name )
           end
 
           # open the current checked out branch
